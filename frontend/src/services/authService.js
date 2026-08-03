dbb8601b9d84fa7d3c65d65bf4ae15107b5074d4
virajlakade <lakadeviraj09@gmail.com>
2026-08-03 18:31:01 +0530

    Ready for deployment

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8090";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const TOKEN_KEY = "studenthub_token";
 export const USER_KEY = "studenthub_user";
